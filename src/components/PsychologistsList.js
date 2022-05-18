0191ff652bb7c6cf8a38be93db48eb94970b3c0c
Richard Tonata <rtonata@gmail.com>
2022-05-18 11:27:19 +0200

    Added Psychologists details

--- src/components/PsychologistsList.js
+++ src/components/PsychologistsList.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Psychologist from './Psychologist';
+import { Link } from 'react-router-dom';
 
 const PsychologistsList = ({ id, name, specialty }) => (
-  <Psychologist id={id} name={name} specialty={specialty} />
+  <div className="grid-cols-3 gap-4 flex items-center">
+    <Link to={`/psychologist/${id}`}>
+      <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 p-2">
+        <div className="flex flex-col items-center pb-10">
+          <img
+            className="mb-3 w-24 h-24 rounded-full shadow-lg"
+            src="https://headshots-inc.com/wp-content/uploads/2020/11/Professional-Headshot-Poses-Blog-Post-1536x1024.jpg"
+            alt="Bonnie"
+          />
+          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+            {name}
+          </h5>
+          <span className="text-center text-sm text-gray-500 dark:text-gray-400">
+            {specialty}
+          </span>
+        </div>
+      </div>
+    </Link>
+  </div>
 );
 
 PsychologistsList.propTypes = {
