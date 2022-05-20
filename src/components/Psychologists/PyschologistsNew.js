4fe4eb2100ce81fef1c9ce01ddd498bd6a062e83
Richard Tonata <rtonata@gmail.com>
2022-05-20 09:21:09 +0200

    Add form to capture new psychologists

--- src/components/Psychologists/PyschologistsNew.js
+++ src/components/Psychologists/PyschologistsNew.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { addPsychologists } from '../../redux/psychologists/psychologists';
 
 const PsychologistsNew = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [gender, setGender] = useState('');
   const [image, setImage] = useState('');
-  const [areaOfSpecialty, setareaOfSpecialty] = useState('');
+  const [areaOfSpecialty, setAreaOfSpecialty] = useState('');
 
   const dispatch = useDispatch();
 
-  const addPsychologist = () => {
+  const addNewPsychologist = () => {
     const newPsychologist = {
       name,
       email,
@@ -21,8 +22,65 @@ const PsychologistsNew = () => {
 
     // dispatch an action and pass it the newBook object (your action's payload)
     // dispatch(addBook(newBook));
-    dispatch(postBook(newBook));
+    dispatch(addPsychologists(newPsychologist));
   };
+
+  return (
+    <div className="card-body">
+      <form>
+        <div className="row">
+          <p className="add-book-title">Please complete the fields below:</p>
+          <div className="col-md-6">
+            <input
+              type="text"
+              placeholder="Name"
+              className="form-control"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="col-md-4">
+            <select
+              name="gender"
+              className="form-control"
+              onChange={(e) => setGender(e.target.value)}>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          </div>
+          <div className="col-md-6">
+            <input
+              type="text"
+              placeholder="Email"
+              className="form-control"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="col-md-6">
+            <input
+              type="textarea"
+              placeholder="Area of specialty"
+              className="form-control"
+              name="areaOfSpecialty"
+              value={areaOfSpecialty}
+              onChange={(e) => setAreaOfSpecialty(e.target.value)}
+            />
+          </div>
+          <div className="col-md-2">
+            <button
+              type="button"
+              className="btn btn-primary btn-update-progress"
+              onClick={addNewPsychologist}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default PsychologistsNew;
