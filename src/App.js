1729681e029f2cb4eef16a6cfe8baf120569b12c
Richard Tonata <rtonata@gmail.com>
2022-05-20 11:38:30 +0200

    Add social media icons to nav panel

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   }, []);
 
   const psychologistSelector = useSelector(
-    (state) => state.psychologistsReducer,
+    (state) => state.psychologistsReducer
   );
 
   const headerInforSelector = useSelector((state) => state.headerInfoReducer);
@@ -41,36 +41,41 @@ function App() {
   ));
 
   return (
-    <div className="min-h-screen grid grid-cols-5 gap-3 bg-white">
-      <div className="border-r-2">
-        <Navigation />
-      </div>
-      <div className="col-span-4">
-        <div className="py-12 mt-5 w-75 bg-white border rounded drop-shadow-lg mx-auto">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="lg:text-center">
-              <p className="mt-2 text-3xl leading-8 font-extrabold text-gray-900 sm:text-4xl tracking-widest">
-                {headerInforSelector.heading}
-              </p>
-              <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                {headerInforSelector.subHeading}
-              </p>
-            </div>
-            <div className="mt-10">
-              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-                <Routes>
-                  <Route path="/" element={psychologists} />
-                  <Route path="/my-appointments" element={<MyAppointments />} />
-                  <Route
-                    path="/psychologist/:id"
-                    element={<PsychologistDetails />}
-                  />
-                  <Route
-                    path="/add-psychologists"
-                    element={<PsychologistsNew />}
-                  />
-                </Routes>
-              </dl>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <Navigation />
+        </div>
+        <div className="col-md-10">
+          <div className="py-12 mt-5 w-75 bg-white border rounded drop-shadow-lg mx-auto">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="lg:text-center">
+                <p className="mt-2 text-3xl leading-8 font-extrabold text-gray-900 sm:text-4xl tracking-widest">
+                  {headerInforSelector.heading}
+                </p>
+                <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                  {headerInforSelector.subHeading}
+                </p>
+              </div>
+              <div className="mt-10">
+                <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                  <Routes>
+                    <Route path="/" element={psychologists} />
+                    <Route
+                      path="/my-appointments"
+                      element={<MyAppointments />}
+                    />
+                    <Route
+                      path="/psychologist/:id"
+                      element={<PsychologistDetails />}
+                    />
+                    <Route
+                      path="/add-psychologists"
+                      element={<PsychologistsNew />}
+                    />
+                  </Routes>
+                </dl>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -25,25 +25,81 @@ const Navigation = () => {
     },
   ];
 
+  const navWidth = {
+    width: 380,
+  };
+
   return (
-    <aside className="min-h-screen" aria-label="Sidebar">
-      <div className="overflow-y-auto align-middle">
-        <ul className="space-y-2">
-          {links.map((link) => (
-            <li key={link.id}>
-              <NavLink
-                to={link.path}
-                className="flex items-center p-2 text-base font-normal text-gray-900 dark:text-white hover:bg-lime-500 hover:text-white dark:hover:bg-gray-700"
-              >
-                <span className="ml-3 font-semibold uppercase">
-                  {link.text}
-                </span>
-              </NavLink>
-            </li>
-          ))}
-        </ul>
+    <div
+      className="d-flex flex-column space-between align-items-stretch flex-shrink-0 bg-white border h-100 d-inline-block"
+      style={navWidth}>
+      <a
+        href="/"
+        className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+        <span className="fs-5 fw-semibold">MENTAL HEALTH</span>
+      </a>
+      <div className="list-group list-group-flush border-bottom scrollarea">
+        {links.map((link) => (
+          <NavLink
+            key={link.id}
+            to={link.path}
+            className="list-group-item list-group-item-action active py-3 lh-sm">
+            <div className="d-flex w-100 align-items-center justify-content-between">
+              <strong className="mb-1">{link.text}</strong>
+            </div>
+          </NavLink>
+        ))}
       </div>
-    </aside>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-twitter"
+          viewBox="0 0 16 16">
+          <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-facebook"
+          viewBox="0 0 16 16">
+          <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-google"
+          viewBox="0 0 16 16">
+          <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-vimeo"
+          viewBox="0 0 16 16">
+          <path d="M15.992 4.204c-.071 1.556-1.158 3.687-3.262 6.393-2.175 2.829-4.016 4.243-5.522 4.243-.933 0-1.722-.861-2.367-2.583L3.55 7.523C3.07 5.8 2.556 4.94 2.007 4.94c-.118 0-.537.253-1.254.754L0 4.724a209.56 209.56 0 0 0 2.334-2.081c1.054-.91 1.845-1.388 2.373-1.437 1.243-.123 2.01.728 2.298 2.553.31 1.968.526 3.19.646 3.666.36 1.631.756 2.446 1.186 2.445.334 0 .836-.53 1.508-1.587.671-1.058 1.03-1.863 1.077-2.415.096-.913-.263-1.37-1.077-1.37a3.022 3.022 0 0 0-1.185.261c.789-2.573 2.291-3.825 4.508-3.756 1.644.05 2.419 1.117 2.324 3.2z" />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-pinterest"
+          viewBox="0 0 16 16">
+          <path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z" />
+        </svg>
+      </div>
+    </div>
   );
 };
 
